19ff1124ed105ec6a7404964e1025a15f54f0cc0
Joseph Neihouse <Joseph Neihouse@Viserion>
2021-08-02 16:38:16 -0400

    Hello!

--- Module7Project/src/Class2.java
+++ Module7Project/src/Class2.java
@@ -1,3 +1,5 @@
 public class Class2 {
-
+	public static void main(String args[]) {
+		System.out.println("Hello from Class2");
+	}
 }
--- Module7Project/src/Class3.java
+++ Module7Project/src/Class3.java
@@ -1,4 +1,5 @@
-
 public class Class3 {
-
+	public static void main(String args[]) {
+		System.out.println("Hello from Class3");
+	}
 }
